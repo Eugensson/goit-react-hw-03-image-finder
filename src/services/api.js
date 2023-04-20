1b67fceb3dd538d8e19b8fb771cd19745336331e
Serhii Kulbashnyi <twitch.live.ua@gmail.com>
2023-04-21 00:20:37 +0300

    4

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,7 @@
 export const fetchImagesWithQuery = (searchQuery, page) => {
-  const url = `https://pixabay.com/api/?q=${searchQuery}&page=${page}&key=34223643-b3db1c288a37bd2710c13c9b4&image_type=photo&orientation=horizontal&per_page=12`;
-  return fetch(url)
+  return fetch(
+    `https://pixabay.com/api/?q=${searchQuery}&page=${page}&key=34223643-b3db1c288a37bd2710c13c9b4&image_type=photo&orientation=horizontal&per_page=12`
+  )
     .then(response =>
       response.ok ? response.json() : Promise.reject(response)
     )
